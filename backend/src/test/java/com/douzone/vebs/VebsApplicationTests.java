390b2b23dd9d9e8f9fade6ff9a1ca89b759f9fab
hamjunkyu <hamjunkyu2882@gmail.com>
2026-04-24 16:09:12 +0900

    feat: 화상영어 수강신청 페이지 초기 구현

---
+++ backend/src/test/java/com/douzone/vebs/VebsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.vebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VebsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
